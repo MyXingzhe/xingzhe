9d531f375ca3a5538758dd8a12deb786f3fed1cb
Xingzhe <2676902@qq.com>
2017-09-15 20:20:31 +0800

    debug pru pwm node

--- src/prupwm/src/prupwm.cpp
+++ src/prupwm/src/prupwm.cpp
@@ -81,6 +81,9 @@ void PruPwm::Setup()
     ret = prussdrv_exec_program (0, PRU_BIN_NAME);
     printf("\tINFO: Executing PRU. ret=%d\r\n", ret);
 
+struct prupwm_param *param = Report();
+    printf("flag=0x%x, period=0x%x, cycle0=0x%x, cycle1=0x%x, cycle3=0x%x, cycle4=0x%x\n", param->flag, param->period, param->cycle[0], param->cycle[1], param->cycle[2], param->cycle[3]);
+
     return ;
 }
 
